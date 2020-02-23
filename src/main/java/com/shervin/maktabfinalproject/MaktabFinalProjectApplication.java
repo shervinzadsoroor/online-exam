283ce4642792b24abeeebcc620389e913a68ae94
shervin <shervinzadsoror@gmail.com>
2020-02-23 09:56:31 +0330

    initial commit

---
+++ src/main/java/com/shervin/maktabfinalproject/MaktabFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shervin.maktabfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaktabFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaktabFinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shervin/maktabfinalproject/MaktabFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shervin.maktabfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaktabFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
